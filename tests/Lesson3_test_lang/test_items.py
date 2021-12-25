b6c06cc1860f3c58dc086cb82aeae6be6b5b7e8c
Павел <pavVvka@gmail.com>
2021-12-25 21:30:29 +0300

    final changes in conftest.py and test_items.py

---
+++ tests/Lesson3_test_lang/test_items.py
@@ -0,0 +1,13 @@
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+link1 = "https://google.com"  # линк без кнопки - для проверки сообщения от assert
+
+
+def test_button_add_to_cart(browser):
+    browser.get(link1)
+    ast = None
+    # time.sleep(10)
+    try:
+        browser.find_element_by_css_selector(".btn-add-to-basket")
+    except:
+        assert ast, ">!< There is no button 'Add To Cart' at page"
